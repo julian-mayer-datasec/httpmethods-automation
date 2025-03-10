ddbb19db6bd4f9363b57be7bb2e1d4d271e919e4
Julian Mayer <julian.mayer@data-sec.net>
2025-03-10 06:00:24 -0400

    Changed to colorized output & Refactoring

--- TestDirbURLs.py
+++ TestDirbURLs.py
@@ -1,17 +1,14 @@
 import re
 import subprocess
-import os
-from typing import List
+import pty
 
-#os.path.dirname(os.path.realpath(__file__))
 
-# Defining main function
-def getURLSfromDirbResults(path:str):
+def getURLsFromDirbResults(path: str):
     urls = []
     file = open(path, "r")
     for line in file:
         if "401" in line or "403" in line:
-            #print(line)
+            # print(line)
             match = re.search(r'(https?:\/\/.*) \(', line)
             url = match.group(1)
             urls.append(url)
@@ -19,6 +16,7 @@ def getURLSfromDirbResults(path:str):
     return urls
 
 
+### Output is not colorized ###
 def startHTTP_Methods_Tester(urls: list[str]):
     for url in urls:
         print(f"--- Test {url} ---\n")
@@ -30,8 +28,14 @@ def startHTTP_Methods_Tester(urls: list[str]):
             print(f"Command failed with return code {e.returncode}")
 
 
-if __name__=="__main__":
-    dirbfile = input("Enter path to dirb result file: ")
-    urls = getURLSfromDirbResults(dirbfile)
+def startHTTP_Methods_Tester_colorized(urls: list[str]):
+    for url in urls:
+        print(f"--- Test {url} ---\n")
+        pty.spawn(["python3", "httpmethods.py", "-k", "-au", url])
+
+
+if __name__ == "__main__":
+    dirbFile = input("Enter path to dirb result file: ")
+    urls = getURLsFromDirbResults(dirbFile)
     print(f"Following URLs will be probed: {urls}\n")
-    startHTTP_Methods_Tester(urls)
+    startHTTP_Methods_Tester_colorized(urls)
